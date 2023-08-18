11bb26fe3193b55d8cff67827e6c07aa03f8211a
Nikita <Nikita.kuzin.88@mail.ru>
2023-08-18 21:47:37 +0500

    add Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.

---
+++ seminar2/task1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write (message) ;
+string vatue  = Console.ReadLine() ;
+int result = Convert.ToInt32(vatue) ;
+return result;
+
+}
+
+int GetThirdRank(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool VatidateNumber (int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третья цифра нет");
+        return false;
+    }
+    return true;
+
+}
+int number = Prompt("Введите число  > ");
+    if (VatidateNumber(number) )
+    {
+        Console .WriteLine (GetThirdRank (number) );
+    }
+
+
+
