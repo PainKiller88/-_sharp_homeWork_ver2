c64b53f6e85a9bcf148ce75af18eeaea79cb7a0c
Nikita <Nikita.kuzin.88@mail.ru>
2023-08-18 20:34:18 +0500

    add Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.

---
+++ seminar2/task0/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+
+Console.Write("Введите трёхзначное число: ");
+
+int number = int.Parse(Console.ReadLine()!);
+
+int secondDigit = number / 10 % 10;
+
+Console.WriteLine($"Вторая цифра: {secondDigit}");
