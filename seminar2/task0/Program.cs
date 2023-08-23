39b8e07dbf45d8fe7ded6ae301b5ee70d24e87f4
Nikita <Nikita.kuzin.88@mail.ru>
2023-08-23 19:51:30 +0500

    add Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.

--- seminar2/task0/Program.cs
+++ seminar2/task0/Program.cs
@@ -4,20 +4,23 @@
 // 78 -> 8
 // 12-> 2
 // 85 -> 8
-Console.Clear();
-int MaxDigit (int num)
-{
-    int firstDigit = num / 10;
-    int secondDigit = num % 10;
-    if (firstDigit > secondDigit)
-    {
-        return firstDigit;
-    }
-    else
-    {
-        return secondDigit;
-    }
-}
-int number =new Random().Next(10, 100); // Случайное число из отрезка [10, 99)
-int res = MaxDigit(number);
-Console.WriteLine($"В числе {number} максимальная цифра {res}");
+
+// Console.Clear();
+// int MaxDigit (int num)
+// {
+//     int firstDigit = num / 10;
+//     int secondDigit = num % 10;
+//     if (firstDigit > secondDigit)
+//     {
+//         return firstDigit;
+//     }
+//     else
+//     {
+//         return secondDigit;
+//     }
+// }
+// int number =new Random().Next(10, 100); // Случайное число из отрезка [10, 99)
+// int res = MaxDigit(number);
+// Console.WriteLine($"В числе {number} максимальная цифра {res}");
+
+//123>13
---
+++ seminar2/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Console.Clear();
+int DelSecondDig(int number)
+{
+    int firstDigit = number / 100;
+    int secondDigit = number % 10;
+
+    int result = firstDigit * 10 + secondDigit;
+    return result;
+}
+
+int randomNumber = new Random().Next(100, 1000);
+
+System.Console.WriteLine($"(randomNumber) - > {DelSecondDig(randomNumber)}");
