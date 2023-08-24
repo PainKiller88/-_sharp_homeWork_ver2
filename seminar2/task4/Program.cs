e2df5465e35a4172453f8c3fc11ff20aa2607fd2
Nikita <Nikita.kuzin.88@mail.ru>
2023-08-24 15:19:01 +0500

    Напишите программу, которая принимает на вход координаты точки (X и Y), // причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.

---
+++ seminar2/task4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+void IsSquare(int num1, int num2)
+{
+    if (num1 * num1 == num2)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+
+int firstNum = InputNum("Введите первое число: ");
+int secondNum = InputNum("Введите второе число: ");
+
+IsSquare(firstNum, secondNum);
