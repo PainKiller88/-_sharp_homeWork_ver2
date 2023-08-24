c261318011ac4c903f71931d6002dcbf3168836e
Nikita <Nikita.kuzin.88@mail.ru>
2023-08-24 12:16:17 +0500

    Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно

---
+++ seminar2/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Clear();
+int InputNum(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+bool Test(int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+int num = InputNum("Введите число: ");
+
+System.Console.WriteLine(Test(num));
