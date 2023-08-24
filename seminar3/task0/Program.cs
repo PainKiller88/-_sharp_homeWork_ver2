bba210842ea336eaa7932ce725aee8a872a3a1e7
Nikita <Nikita.kuzin.88@mail.ru>
2023-08-24 16:00:25 +0500

    add Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.

---
+++ seminar3/task0/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int Qudrant(int axisX, int axisY)
+{
+    int num = 0;
+    // if (axisX > 0 && axisY > 0)
+    //     num = 1;
+    // else if (axisX < 0 && axisY > 0)
+    //     num = 2;
+    // else if (axisX < 0 && axisY < 0)
+    //     num = 3;
+    // else if (axisX > 0 && axisY < 0)
+    //     num = 4;
+    // return num;
+    if (axisX > 0)
+    {
+        if (axisX > 0) num = 1;
+        else num = 4;
+    }
+    else if (axisX < 0)
+    {
+        if (axisY > 0) num = 2;
+        else num = 3;
+    }
+    return num;
+}
+
+int x = InputNum("Введите координату X: ");
+
+int y = InputNum("Введите координату Y: ");
+
+int res = Qudrant(x, y);
+if (res == 0)
+    Console.WriteLine("Точка лежит на границе четвертей");
+else
+    Console.WriteLine($"Точка с координатами ({x}, {y}) лежит в четверти {res}");
---
+++ seminar3/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+int InputMax(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+double PointDistance(int AX, int AY, int BX, int BY)
+{
+    double distance = 0;
+    distance = Math.Sqrt(Math.Pow(AX - BX, 2) + Math.Pow(AY - BY, 2));
+    return distance;
+}
+
+int AX = InputMax("Введите координату X точки A: ");
+
+int AY = InputMax("Введите координату Y точки A: ");
+
+int BX = InputMax("Введите координату X точки B: ");
+
+int BY = InputMax("Введите координату Y точки B: ");
+Console.WriteLine(PointDistance(AX, AY, BX, BY));
