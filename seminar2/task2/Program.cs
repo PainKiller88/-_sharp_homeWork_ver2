7b50234eafecbb90b8ddee5335a604c9978dc697
Nikita <Nikita.kuzin.88@mail.ru>
2023-08-24 12:02:09 +0500

    add Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.

---
+++ seminar2/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+bool Multiplicity(int firstNum, int secondNum)
+
+{
+    // if (firstNum % secondNum == 0)
+    //     return true;
+    // else
+    //     return false;
+    return firstNum % secondNum == 0;
+}
+
+int firstNum = InputNum("Введите первое число: ");
+int secondNum = InputNum("Введите второе число: ");
+
+bool check = Multiplicity(firstNum, secondNum);
+Console.WriteLine(check);
