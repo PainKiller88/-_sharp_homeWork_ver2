16979aaf231313fda72917d75d32d2b97be9f8a0
Nikita <Nikita.kuzin.88@mail.ru>
2023-08-26 15:50:38 +0500

    add Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел // от 1 до N.

---
+++ seminar4/task0/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SumNum(int num)
+{
+    int sum = 0;
+    for (int i = 0; i <= num; i++)
+
+        sum += i;
+    return sum;
+}
+
+int num = InputNum("Введите число: ");
+int sum = SumNum(num);
+Console.WriteLine(sum);
