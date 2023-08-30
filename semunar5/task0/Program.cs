99d80960c6a334a3c808459ca38749770dd0a3a7
Nikita <Nikita.kuzin.88@mail.ru>
2023-08-30 19:30:57 +0500

    add task0 Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.

---
+++ semunar5/task0/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int GetUserInput(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] FillArrayWithPositiveThreeDigitNumbers(int length)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
+
+    return array;
+}
+
+string ArrayToString(int[] array)
+{
+    string result = "[";
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        result += array[i] + ", ";
+    }
+    result += array[array.Length - 1] + "]";
+    return result;
+}
+
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int length = GetUserInput("Введите длину массива: ");
+int[] array = FillArrayWithPositiveThreeDigitNumbers(length);
+Console.WriteLine("Массив: " + ArrayToString(array));
+int count = CountEvenNumbers(array);
+Console.WriteLine("Количество четных чисел: " + count);
+
+
